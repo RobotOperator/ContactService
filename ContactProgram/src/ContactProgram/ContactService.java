9de8a10276f6354885ab807d56e4366c807aed5b
Cainl <Cainl@DESKTOP-9TNULAG.lan>
2023-04-01 21:54:54 -0700

    Working insert contact and connection to database.

--- ContactProgram/src/ContactProgram/ContactService.java
+++ ContactProgram/src/ContactProgram/ContactService.java
@@ -1,6 +1,6 @@
 package ContactProgram;
 
-//import ContactProgram.Contact;
+import java.io.File;
 import java.util.ArrayList;
 
 public class ContactService {
@@ -8,11 +8,24 @@ public class ContactService {
 	//Create new array lists to hold the contacts created and unique ID fields.
 	private ArrayList<Contact> ContactList;
 	private ArrayList<String> Ids;
+	private dbConnector db;
 	
 	//Default constructor with blank array lists.
 	public ContactService() {
+		//Statically configured path to the database file
+		String dbDirectory = "";
+		String dbFile = "contact.db";
+		File dbPath = new File(dbDirectory + dbFile);
 		ContactList = new ArrayList<Contact>();
 		Ids = new ArrayList<String>();
+		//Test if the database exists already
+		boolean fileExists = dbPath.exists();
+		//Create a new database connection
+		db = new dbConnector(dbDirectory, dbFile);
+		//If the file didn't exist then create the table to hold contacts
+		if (!fileExists) {
+			db.createContactTable();
+		}
 	}
 	
 	//Method to add a new contact, checks if a valid unique id has been provided. Class will throw an error if the arguments are bad.
@@ -24,6 +37,7 @@ public class ContactService {
 		Contact newone = new Contact(id, firstName, lastName, phoneNumber, address);
 		Ids.add(id);
 		ContactList.add(newone);
+		this.db.insertContact(id, firstName, lastName, phoneNumber, address);
 		}
 	}
 	
--- ContactProgram/src/ContactProgram/dbConnector.java
+++ ContactProgram/src/ContactProgram/dbConnector.java
@@ -2,10 +2,11 @@ package ContactProgram;
 
 import java.util.ArrayList;
 import java.sql.Connection;
-//import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * 
@@ -22,9 +23,10 @@ public class dbConnector {
 	//Default constructor, requires directory name and filename for connection
 	public dbConnector (String directory, String filename) {
 		String url = "jdbc:sqlite:" + directory + filename;
-		try (Connection new_conn = DriverManager.getConnection(url)) {
-			this.conn = new_conn;
-			System.out.println("Connected db : " + directory + filename);
+		try {
+			//this.conn = new_conn;
+			this.conn = DriverManager.getConnection(url);
+			System.out.println("Connected database : " + directory + filename);
 		}
 		catch (SQLException e) {
 			System.out.println(e.getMessage()); //print error message to console
@@ -32,8 +34,23 @@ public class dbConnector {
 		}
 	}
 	
-	public static void insertContact() {
-		//stub
+	public void insertContact(String id, String firstName, String lastName, String phoneNumber, String address) {
+		//prepare statement string 
+		String sql = "INSERT INTO contacts(id, first_name, last_name, phone_number, address) VALUES(?,?,?,?,?)";
+		
+		//Assign parameters to values for prepared statement
+		try(PreparedStatement pstmt = this.conn.prepareStatement(sql)) {
+			pstmt.setString(1, id);
+			pstmt.setString(2, firstName);
+			pstmt.setString(3, lastName);
+			pstmt.setString(4, phoneNumber);
+			pstmt.setString(5, address);
+			pstmt.executeUpdate(); //execute the insert statement
+			pstmt.close(); //close and release resources to be used in subsequent queries
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	public static void deleteContact() {
@@ -44,12 +61,30 @@ public class dbConnector {
 		//stub
 	}
 	
-	public ArrayList<Contact> retrieveContacts() {
-		//stub
+	//Method to load contacts from the database
+	public void retrieveContacts() {
+		//Create new ArrayList of contacts
+		//ArrayList<Contact> databaseContacts = new ArrayList<Contact>();
+		//Prepare SQL query
+		String sql = "SELECT * FROM contacts";
+		
+		
+		try (Statement stmt = this.conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+			//loop through the result set
+			while (rs.next()) {
+				Contact newone = new Contact(rs.getString("id"), rs.getString("first_name"), rs.getString("last_name"), rs.getString("phone_number"), rs.getString("address"));
+				databaseContacts
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+			throw new IllegalStateException("Contacts could not be loaded from database");
+		}
 		return new ArrayList<Contact>(); //default empty array list
 	}
 	
-	//Method to create the Contact table in the db
+	//Method to create the Contact table in the database
 	public void createContactTable() {
 		//pass the connection in 
 		Connection connected = this.conn;
